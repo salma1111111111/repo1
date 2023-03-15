8556efa0d4ceb0fdcbd4c7ffdb7cbf6204b32fe7
salma1111111111 <salmaelallali3@gmail.com>
2023-03-15 14:54:18 +0000

    commit

--- app.py
+++ app.py
@@ -4,9 +4,7 @@
 def inc(x_val1):
     '''fonction 1'''
     return x_val1 + 1
-def addition(a_num,b_num):
-    '''fonction 2'''
-    return a_num + b_num
+
 def deinc(x_val2):
     '''fonction 3'''
     return x_val2 - 1
